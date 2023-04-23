781f1e9907327abe0275c4139c2f023e47f48829
AnaNayy <anastasiia_usenko@hotmail.com>
2023-04-23 15:34:31 +0300

    add layout basic

--- src/route/index.js
+++ src/route/index.js
@@ -1069,7 +1069,7 @@ router.get('/task21', function (req, res) {
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('task21', {
 
-    layout:'bootstrap',
+    layout:'basic',
 
     heading: {
       main: {
